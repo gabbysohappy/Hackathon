c6c9f36370af909456bee6fa8a33cd1b01ae6702
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2019-12-07 14:55:53 -0700

    start on comments contr

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,57 @@
 class CommentsController < ApplicationController
+  before_action :set_movie
+  before_action :set_comment, only: [:edit, :update, :show, :destroy]
+
   def index
+    @comments = @movie.comments.all
   end
 
   def show
   end
 
   def new
+    @movies = Movie.all
+    @comment = @movie.comments.new
   end
 
+  def create
+    @comment = @movie.comments.new
+    if @comment.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
+
   def edit
+    @comments = Comment.all
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to movie_path(@movie)
+  end
+
+  private 
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end 
+
+  def set_comment
+    @comment = @movie.comments.find(params[:id])
+  end
+  
+  def comment_params 
+    params.require(:comment).permit(:body)
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def index
+
   end
 
   def show
@@ -8,6 +11,22 @@ class MoviesController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
   end
+
+  def update
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :duration, :genre, :description, :trailer)
+  end
+  
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end 
 end
